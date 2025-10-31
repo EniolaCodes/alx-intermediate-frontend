e5c40edf2af8765f3b45fd9ff85e7ac2223ce9da
EniolaCodes <adebimpeniola@gmail.com>
2025-11-01 00:49:36 +0100

    feat: add 0x02-tailwind-css folder

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // include HTML files in the root of this folder and subfolders
+  content: ["./**/*.{html,js}", "../**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
